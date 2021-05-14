9c622bb3d2633bd30be7db6512a24c25fde13d40
LeonidVolohov <leka.volohov@gmail.com>
2021-05-14 22:47:22 +0300

    API-Migration:  - Hid the API-keys

--- app/src/main/java/com/volohov/currency/api/ApiConstants.kt
+++ app/src/main/java/com/volohov/currency/api/ApiConstants.kt
@@ -1,13 +1,16 @@
 package com.volohov.currency.api
 
+import com.volohov.currency.BuildConfig
+
+
 class ApiConstants {
     companion object {
         const val CURRENCY_API_ALPHAVANTAGE_BASE_URL = "https://www.alphavantage.co/"
-        const val CURRENCY_API_KEY_DATE = "LBWSCWL8M5V6V9T0"
-        const val CURRENCY_API_KEY_TARGETRATE_PRICE = "OEGLRPXLQZU6KGB8"
-        const val CURRENCY_API_KEY_PERIOD_DATA = "QK5BJQVB98T0NXCV"
+        const val CURRENCY_API_KEY_DATE = BuildConfig.CURRENCY_API_KEY_DATE
+        const val CURRENCY_API_KEY_TARGETRATE_PRICE = BuildConfig.CURRENCY_API_KEY_TARGETRATE_PRICE
+        const val CURRENCY_API_KEY_PERIOD_DATA = BuildConfig.CURRENCY_API_KEY_PERIOD_DATA
 
         const val CURRENCYLAYER_BASE_URL = "https://api.currencylayer.com/"
-        const val CURRENCYLAYER_API_KEY = "d3109fb32ca12e4226b1613f3329f622"
+        const val CURRENCYLAYER_API_KEY = BuildConfig.CURRENCYLAYER_API_KEY
     }
 }
