490cc6a0050d861ba21dd3650f4b3d8d9fbffc19
LeonidVolohov <leka.volohov@gmail.com>
2021-05-20 19:03:55 +0300

    BottomNavigationView:  - Added bottom navigation view  - Added three empty fragments for chart, currency and prediction screens  - Almost finished fragment_currency.xml file

---
+++ app/src/main/java/com/volohov/currency/ui/MainActivityBottom.kt
@@ -0,0 +1,54 @@
+package com.volohov.currency.ui
+
+import android.gesture.Prediction
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.volohov.currency.ChartFragment
+import com.volohov.currency.CurrencyFragment
+import com.volohov.currency.PredictionFragment
+import com.volohov.currency.R
+import kotlinx.android.synthetic.main.activity_main_bottomnavview.*
+
+class MainActivityBottom : AppCompatActivity() {
+    private lateinit var selectedFragment: Fragment
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_bottomnavview)
+
+        selectedFragment = CurrencyFragment()
+
+        bottom_navigation_view.selectedItemId = R.id.currency_tab
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, selectedFragment)
+            .commitNow()
+
+        bottom_navigation_view.selectedItemId = R.id.currency_tab
+        bottom_navigation_view.setOnNavigationItemSelectedListener(navigationListener)
+    }
+
+    private val navigationListener: BottomNavigationView.OnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            selectedFragment = when(item.itemId) {
+                R.id.chart_tab -> {
+                    ChartFragment()
+                }
+
+                R.id.currency_tab -> {
+                    CurrencyFragment()
+                }
+
+                R.id.prediction_tab -> {
+                    PredictionFragment()
+                }
+
+                else -> TODO("?")
+            }
+
+            true
+        }
+}
