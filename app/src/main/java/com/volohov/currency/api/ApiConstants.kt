24d5377daa1535c71f70e1f92e9a595a0e143ebf
LeonidVolohov <leka.volohov@gmail.com>
2021-05-30 11:16:53 +0300

    BottomNavigationView:  - Small fix with currency constant

--- app/src/main/java/com/volohov/currency/api/ApiConstants.kt
+++ app/src/main/java/com/volohov/currency/api/ApiConstants.kt
@@ -5,7 +5,7 @@ import com.volohov.currency.BuildConfig
 
 class ApiConstants {
     companion object {
-        const val CURRENCY_API_ALPHAVANTAGE_BASE_URL = "https://www.alphavantage.co/"
+        const val CURRENCY_API_ALPHAVANTAGE_BASE_URL = "https://www.alphavantage.com/"
         const val CURRENCY_API_KEY_DATE = BuildConfig.CURRENCY_API_KEY_DATE
         const val CURRENCY_API_KEY_TARGETRATE_PRICE = BuildConfig.CURRENCY_API_KEY_TARGETRATE_PRICE
         const val CURRENCY_API_KEY_PERIOD_DATA = BuildConfig.CURRENCY_API_KEY_PERIOD_DATA
